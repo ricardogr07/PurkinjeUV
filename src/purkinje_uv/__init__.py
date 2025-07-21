0c32f63ec62c68f164f77a72b6ea3c698275a631
Ricardo García Ramírez <rgr.5882@gmail.com>
2025-07-20 21:05:46 -0600

    fix: update imports and __all__ in __init__.py for consistency

--- src/purkinje_uv/__init__.py
+++ src/purkinje_uv/__init__.py
@@ -1,6 +1,8 @@
-from .FractalTreeUV import FractalTree
-from .FractalTreeUV import Parameters
-from .PurkinjeTree import PurkinjeTree
-from .Mesh import Mesh
+from .edge import Edge
+from .fractal_tree_uv import FractalTree
+from .fractal_tree_parameters import Parameters
+from .igb_reader import IGBReader
+from .mesh import Mesh
+from .purkinje_tree import PurkinjeTree
 
-__all__ = ["PurkinjeTree", "FractalTree", "Parameters", "Mesh"]
+__all__ = ["Edge", "FractalTree", "Mesh", "Parameters", "purkinje_tree", "IGBReader"]
