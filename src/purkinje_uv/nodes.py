d42ba06e40f902ead10a6aec281ac6317c729e55
Ricardo García Ramírez <rgr.5882@gmail.com>
2025-08-03 00:36:51 -0600

    refactor: enhance type hints and improve method signatures in Nodes class

--- src/purkinje_uv/nodes.py
+++ src/purkinje_uv/nodes.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.spatial import cKDTree
+from typing import Any, List, Sequence, Tuple, Union
+from numpy.typing import NDArray
 
 
 class Nodes:
@@ -17,14 +19,17 @@ class Nodes:
 
     """
 
-    def __init__(self, init_node):
-        self.nodes = []
-        self.nodes.append(init_node)
+    nodes: List[NDArray[Any]]
+    end_nodes: List[int]
+    tree: cKDTree
+
+    def __init__(self, init_node: NDArray[Any]) -> None:
+        self.nodes = [init_node]
         self.last_node = 0
         self.end_nodes = []
         self.tree = cKDTree(self.nodes)
 
-    def add_nodes(self, queue):
+    def add_nodes(self, queue: Sequence[NDArray[Any]]) -> List[int]:
         """This function stores a list of nodes of a branch and returns the node indices. It also updates the tree to compute distances.
 
         Args:
@@ -33,7 +38,7 @@ class Nodes:
         Returns:
             nodes_id (list): the indices of the added nodes.
         """
-        nodes_id = []
+        nodes_id: List[int] = []
         for point in queue:
             self.nodes.append(point)
             self.last_node += 1
@@ -41,7 +46,7 @@ class Nodes:
         self.tree = cKDTree(self.nodes)
         return nodes_id
 
-    def distance_from_point(self, point):
+    def distance_from_point(self, point: Union[NDArray[Any], Sequence[float]]) -> float:
         """This function returns the distance from any point to the closest node in the tree.
 
         Args:
@@ -50,10 +55,11 @@ class Nodes:
         Returns:
             d (float): the distance between point and the closest node in the tree.
         """
-        d, node = self.tree.query(point)
+        res = self.tree.query(point)
+        d = float(res[0])
         return d
 
-    def distance_from_node(self, node):
+    def distance_from_node(self, node: int) -> float:
         """This function returns the distance from any node to the closest node in the tree.
 
         Args:
@@ -62,44 +68,44 @@ class Nodes:
         Returns:
             d (float): the distance between specified node and the closest node in the tree.
         """
-        d, node = self.tree.query(self.nodes[node])
+        res = self.tree.query(self.nodes[node])
+        d = float(res[0])
         return d
 
-    def update_collision_tree(self, nodes_to_exclude):
-        """This function updates the collision_tree excluding a list of nodes from all the nodes in the tree. If all the existing nodes are excluded, one distant node is added.
+    def update_collision_tree(self, nodes_to_exclude: Sequence[int]) -> None:
+        """
+        Update the collision_tree by excluding specified nodes.
+        If no nodes remain, insert one distant dummy point so the KD‐tree never ends up empty.
 
         Args:
-            nodes_to_exclude (list): contains the nodes to exclude from the tree. Usually it should be the mother and the brother branch nodes.
-
-        Returns:
-            none
+            nodes_to_exclude (Sequence[int]): Indices of nodes to exclude.
         """
-        nodes = set(range(len(self.nodes)))
+
+        nodes: set[int] = set(range(len(self.nodes)))
         nodes = nodes.difference(nodes_to_exclude)
-        nodes_to_consider = [self.nodes[x] for x in nodes]
-        self.nodes_to_consider_keys = [x for x in nodes]
+        nodes_to_consider: List[NDArray[Any]] = [self.nodes[i] for i in nodes]
+        self.nodes_to_consider_keys = list(nodes)
+
         if len(nodes_to_consider) == 0:
-            nodes_to_consider = [
-                np.array([-100000000000.0, -100000000000.0, -100000000000.0])
-            ]
+            dummy: NDArray[Any] = np.array([-1e11, -1e11, -1e11], dtype=float)
+            nodes_to_consider = [dummy]
             self.nodes_to_consider_keys = [100000000]
             print("no nodes to consider")
+
         self.collision_tree = cKDTree(nodes_to_consider)
 
-    def collision(self, point):
-        """This function returns the distance between one point and the closest node in the tree and the index of the closest node using the collision_tree.
-
-        Args:
-            point (array): the coordinates of the point to calculate the distance from.
-
-        Returns:
-            collision (tuple): (distance to the closest node, index of the closest node)
+    def collision(
+        self, point: Union[NDArray[Any], Sequence[float]]
+    ) -> Tuple[int, float]:
         """
-        d, node = self.collision_tree.query(point)
-        collision = (self.nodes_to_consider_keys[node], d)
-        return collision
+        Return (node_index, distance) for the closest node to `point` using the collision_tree.
+        """
+        res = self.collision_tree.query(point)
+        d = float(res[0])
+        idx = int(res[1])
+        return (self.nodes_to_consider_keys[idx], d)
 
-    def gradient(self, point):
+    def gradient(self, point: Union[NDArray[Any], Sequence[float]]) -> NDArray[Any]:
         """This function returns the gradient of the distance from the existing points of the tree from any point. It uses a central finite difference approximation.
 
         Args:
@@ -109,16 +115,21 @@ class Nodes:
             grad (array): (x,y,z) components of gradient of the distance.
         """
 
-        if True:
-            point = np.array(point)
-            d, idx = self.tree.query(point)
-            if np.isclose(d, 0.0):
-                return np.array([0, 0, 0])
-            p2 = self.nodes[idx]
-            # print(point,p2,self.tree.query(point))
-            grad2 = (point - p2) / np.linalg.norm(p2 - point)
-            return grad2
+        arr = np.array(point, dtype=float)
+        res = self.tree.query(arr)
+        d = float(res[0])
+        idx = int(res[1])
 
+        if np.isclose(d, 0.0):
+            zero_grad: NDArray[Any] = np.array([0.0, 0.0, 0.0], dtype=float)
+            return zero_grad
+
+        p2 = self.nodes[idx]
+        diff = arr - p2
+        grad2: NDArray[Any] = diff / np.linalg.norm(p2 - arr)
+        return grad2
+
+        # TODO is this deprecated?
         delta = 0.001
         dx = np.array([delta, 0, 0])
         dy = np.array([0.0, delta, 0.0])
