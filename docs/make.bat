464c78102cc170acb5b71896e178a998e5487cf2
Ricardo García Ramírez <rgr.5882@gmail.com>
2025-08-17 14:07:05 -0600

    feat: update Sphinx configuration and dependencies for improved documentation support

--- docs/make.bat
+++ docs/make.bat
@@ -1,35 +1,20 @@
 @ECHO OFF
 
-pushd %~dp0
-
-REM Command file for Sphinx documentation
-
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
-)
+REM -- paths
 set SOURCEDIR=.
 set BUILDDIR=_build
 
-%SPHINXBUILD% >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
-	exit /b 1
+REM -- Force sphinx-multiversion as the builder
+set SPHINXBUILD=sphinx-multiversion
+
+IF "%1" == "html" (
+    %SPHINXBUILD% -a -E -W --keep-going %SOURCEDIR% %BUILDDIR%/html
+    GOTO end
 )
 
-if "%1" == "" goto help
-
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-goto end
-
-:help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+IF "%1" == "" (
+    ECHO.Please use `make.bat html`
+    GOTO end
+)
 
 :end
-popd
